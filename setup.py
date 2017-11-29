2ecb5744f5434e33cd69ffdd98803b818b8e5e88
Bill Allen <photo.allen@gmail.com>
2017-11-29 16:34:27 +0000

    Upped micro version number since github doesn't seem to allow bad tags to be deleted.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='ipydeps',
-    version='0.5.0',
+    version='0.5.1',
     author='Bill Allen',
     author_email='photo.allen@gmail.com',
     description='A pip interface wrapper for installing packages from within Jupyter notebooks.',
     license='MIT',
     keywords='pip install setup jupyter notebook dependencies'.split(),
     url='https://github.com/nbgallery/ipydeps',
-    download_url='https://github.com/nbgallery/ipydeps/archive/0.5.0.tar.gz',
+    download_url='https://github.com/nbgallery/ipydeps/archive/0.5.1.tar.gz',
     packages=['ipydeps'],
     install_requires=['pip', 'setuptools'],
     dependency_links=[],
