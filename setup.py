624efeba669e8d7b69b66ac45d88d44be5594406
Bill Allen <photo.allen@gmail.com>
2018-04-18 05:46:54 +0100

    Limiting pip to version until a fix for pip 10 is implemented.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ipydeps',
-    version='0.6.0',
+    version='0.6.1',
     author='Bill Allen',
     author_email='photo.allen@gmail.com',
     description='A pip interface wrapper for installing packages from within Jupyter notebooks.',
@@ -12,7 +12,7 @@ setup(
     keywords='pip install setup jupyter notebook dependencies'.split(),
     url='https://github.com/nbgallery/ipydeps',
     packages=['ipydeps'],
-    install_requires=['pip', 'setuptools', 'pypki2>=0.10.0'],
+    install_requires=['pip<10', 'setuptools', 'pypki2>=0.10.0'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Topic :: Utilities',
