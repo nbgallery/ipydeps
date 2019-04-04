250464e42322085c86c3ccba06939358272cc5a7
Bill Allen <photo.allen@gmail.com>
2019-04-04 21:17:04 +0100

    Incremented micro version number and added long_description.

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
+    long_description = f.read()
+
 setup(
     name='ipydeps',
-    version='0.8.1',
+    version='0.8.2',
     author='Bill Allen',
     author_email='photo.allen@gmail.com',
     description='A pip interface wrapper for installing packages from within Jupyter notebooks.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     keywords='pip install setup jupyter notebook dependencies'.split(),
     url='https://github.com/nbgallery/ipydeps',
